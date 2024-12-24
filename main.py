fad9f83ed4ffd8a165333e69ae99ef53be42e96c
McBossWithUwULoveUwUForever <168752421+LoveUwUBotForeverMcBoss1999@users.noreply.github.com>
2024-12-24 12:17:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ from flask import Flask, send_from_directory, abort
 
 app = Flask(__name__)
 
+# Serve static files like .html, .css, .js, images from the root directory
 @app.route('/', defaults={'filename': 'index.html'})
 @app.route('/<path:filename>')
 def serve_files(filename):
     try:
-        # Serve the requested file from the root directory
         return send_from_directory('.', filename)
-    except:
-        # If the file is not found, return a 404 error
+    except Exception as e:
         abort(404)
 
 if __name__ == '__main__':
