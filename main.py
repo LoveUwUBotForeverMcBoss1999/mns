9a81909b090826b6dda1efef55757ed5665e8322
McBossWithUwULoveUwUForever <168752421+LoveUwUBotForeverMcBoss1999@users.noreply.github.com>
2024-12-24 12:08:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from flask import Flask, send_from_directory
 
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return send_from_directory('static', 'index.html')
+    return send_from_directory('.', 'index.html')  # Serve index.html from the root directory
 
 @app.route('/<path:filename>')
 def static_files(filename):
-    return send_from_directory('static', filename)
+    return send_from_directory('.', filename)  # Serve other files from the root directory
 
 if __name__ == '__main__':
     app.run(debug=True)
